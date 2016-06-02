ee71f7a9b5c11310ee59e4fb81183a52b01a7d62
simplecss <simplecss@yandex.ru>
2016-06-02 19:30:39 +0300

    menu

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,21 @@ export default (function App(window, document, $){
 
 	var isNativeScrollEnabled = true;
 
+	var isMobile = (function() { 
+		if( navigator.userAgent.match(/Android/i)
+		|| navigator.userAgent.match(/webOS/i)
+		|| navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i)
+		|| navigator.userAgent.match(/BlackBerry/i)
+		|| navigator.userAgent.match(/Windows Phone/i)
+		){
+			return true;
+		} else {
+			return false;
+		}
+	})();
+
 	function _scrollMeTo(){
 		
 		$('.js-goto').on('click', function(e){
@@ -208,11 +223,54 @@ export default (function App(window, document, $){
 		$(document).on('scroll', fix);
 	}
 
+
+	function _menu(){
+		var $menuHrefs = $('.menu__href');
+		var $sections = $('.section');
+
+		var winHeight = ( window.innerHeight || document.documentElement.clientHeight );
+
+		function setActive(){
+						
+			$sections.each(function(index, section){
+				
+				var sectionId = $(this).attr('id');
+				var rect = this.getBoundingClientRect();
+				var rectTop = Math.round(rect.top);
+				var rectBottom = Math.round(rect.bottom);
+
+				if (rectTop <= 50 && rectBottom / 2 <= winHeight ){
+					$menuHrefs.removeClass('active');
+					$menuHrefs.filter('[href="#' + sectionId + '"]').addClass('active');
+				}
+
+			});
+		}
+
+		setActive();
+
+		$(window).on('scroll', function(e){
+			setActive();
+		});
+
+		$(window).on('resize', function(e){
+			winHeight = ( window.innerHeight || document.documentElement.clientHeight );
+			
+			setActive();
+		});
+
+	}
+
 	function init(){
-		_header();
-		_sections();
-		_scroll();
+
+		if (!isMobile){
+			_header();
+			_sections();
+			_scroll();
+		}
+
 		_scrollMeTo();
+		_menu();
 	}
 
 	return {
